cdb74b98c1e21b1f968974ef2ab156a71a3f9afb
Max <max_lisyanskiy@mail.ru>
2020-08-18 17:19:30 +0300

    global class

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-import Slider from './modules/slider';
+import MainSlider from './modules/slider/slider-main';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new Slider('.page', '.next');
+    const slider = new MainSlider({btns: '.next', page: '.page'});
     slider.render();
 });
